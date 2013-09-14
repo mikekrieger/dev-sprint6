b0c1ec303b0294cac42826153a683be59dbb2fc1
mikekrieger <mlkrgr@gmail.com>
2013-09-14 19:31:56 -0400

    committing 6

--- chap14.py
+++ chap14.py
@@ -1,1 +1,20 @@
 # This is where chapter 14 exercises go.
+#Name: Michael Krieger
+
+import urllib
+
+zipcode = ''
+
+url = 'http://uszip.com/zip/' + zipcode
+conn = urllib.urlopen(url)
+
+for line in conn.fp:
+    line = line.strip()
+    if 'Population' in line:
+        print line
+    if 'Longitude' in line: 
+        print line
+    if 'Latitude' in line: 
+        print line
+
+conn.close()
--- chap15.py
+++ chap15.py
@@ -1,1 +1,33 @@
 # This is where chapter 15 exercises go.
+
+#ex 15.3
+
+from tkinter import *
+
+class rectangle():
+	def __init__(self, coords, color):   
+		self.coords = coords
+		self.color = color   
+	def __del__(self):
+		print("InDELETE")
+		del self
+		print("Goodbye")
+	def draw(self, canvas):
+		print("In draw")
+		print("Canvas  = ",canvas)
+		print("self = ",self)
+		print("build canvas = ", rectangle_id = Canvas.create_rectangle(*(0, 0, 30, 30), fill="yellow")
+
+root = Tk()
+root.title('Basic Tkinter straight line')
+width = 500
+height = 500
+w = Canvas(root, width=width, height=height)
+
+f = []
+f=Rectangle((0+30*10, 0+30*10, 100+30*10, 100+30*10),"blue")
+print("Draw object",f.draw(w),f)
+f.__del__()
+del f
+w.pack()
+mainloop()
